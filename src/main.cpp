8c09ea0afe9d3b5bc8b3bf2e6d25c78060f3fc39
Pierre Nicolay <nicolaypierre.95@gmail.com>
2021-01-21 23:40:30 +0000

    main funcion and timing

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,37 @@
 #include "cost_base.hpp"
 #include <iostream>
 
+#include <chrono>  // for high_resolution_clock
+
 using namespace std;
 using namespace tensorflow;
 
 int main(int argc, char const *argv[]) {
-    int k(1500), tau(3), sDim(4), aDim(2);
+    int k(1500), tau(100), sDim(4), aDim(2);
+    bool debug(true);
     float dt(0.01);
     ControllerBase ctrl(k, tau, dt, 1., sDim, aDim);
-    Status log = ctrl.mLogGraph();
-    if (log != Status::OK()) {
-        cout << "Writing failed: " << log << endl;
-    } else {
-        cout << "Writing succesful" << endl;
+    if (debug) {
+        Status log = ctrl.mLogGraph();
+        if (log != Status::OK()) {
+            cout << "Writing failed: " << log << endl;
+        } else {
+            cout << "Writing succesful" << endl;
+        }
     }
-    ctrl.test();
+    vector<float> init = {0, 0};
+
+    // Record start time
+    auto start = chrono::high_resolution_clock::now();
+    for (int i=0; i <100; i++){
+        ctrl.next(init);
+    }
+    // Record end time
+    auto finish = chrono::high_resolution_clock::now();
+
+    chrono::duration<double> elapsed = finish - start;
+    cout << elapsed.count()/100.f << endl;
+
 
 
     /* Section to test the cost computational Graph.
