63f7b30f0303194a79ad889e7d2f554a4095bc81
Pierre Nicolay <nicolaypierre.95@gmail.com>
2021-02-15 18:10:22 +0000

    Point mass model tested

--- scripts/test.py
+++ scripts/test.py
@@ -2,22 +2,190 @@ import tensorflow as tf
 from model_base import ModelBase
 from cost_base import CostBase
 from controller_base import ControllerBase
+import numpy as np
 
 class TestModel(tf.test.TestCase):
     def setUp(self):
+        self.dt=0.1
         pass
 
-    def testStep1(self):
-        pass
+    def test_step1_k1_s2_a1_m1(self):
+        k = 1
+        s = 2
+        a = 1
+        m = 1.
+        model = ModelBase(m, self.dt, s, a)
 
-    def testStep2(self):
-        pass
+        state_in = np.array([[[0.], [0.]]])
+        action_in = np.array([[[1.]]])
 
-    def testStep3(self):
-        pass
+        acc = self.dt*self.dt/(2.*m)
+        vel = self.dt/m
+        exp_u = np.array([[[acc], [vel]]])
+        exp_x = np.array([[[0.], [0.]]])
+        exp = exp_u + exp_x
 
-    def testStep4(self):
-        pass
+        x_pred = model.buildFreeStepGraph("", state_in)
+        u_pred = model.buildActionStepGraph("", action_in)
+        pred = model.buildStepGraph("", state_in, action_in)
+
+        self.assertAllClose(exp_u, u_pred)
+        self.assertAllClose(exp_x, x_pred)
+        self.assertAllClose(exp, pred)
+
+    def test_step1_k1_s4_a2_m1(self):
+        k = 1
+        s = 4
+        a = 2
+        m = 1.
+        model = ModelBase(m, self.dt, s, a)
+
+        state_in = np.array([[[0.], [0.], [0.], [0.]]])
+        action_in = np.array([[[1.], [1.]]])
+
+        acc = self.dt*self.dt/(2.*m)
+        vel = self.dt/m
+        exp_u = np.array([[[acc], [vel], [acc], [vel]]])
+        exp_x = np.array([[[0.], [0.], [0.], [0.]]])
+        exp = exp_u + exp_x
+
+        x_pred = model.buildFreeStepGraph("", state_in)
+        u_pred = model.buildActionStepGraph("", action_in)
+        pred = model.buildStepGraph("", state_in, action_in)
+
+        self.assertAllClose(exp_u, u_pred)
+        self.assertAllClose(exp_x, x_pred)
+        self.assertAllClose(exp, pred)
+
+    def test_step1_k5_s6_a3_m1d5(self):
+        k = 5
+        s = 6
+        a = 3
+        m = 1.5
+        model = ModelBase(m, self.dt, s, a)
+
+        state_in = np.array([[[0.], [0.], [0.], [0.], [0.], [0.]],
+                             [[2.], [1.], [5.], [0.], [-1.], [-2.]],
+                             [[0.5], [0.5], [0.5], [0.5], [0.5], [0.5]],
+                             [[1.], [0.], [1.], [0.], [1.], [0.]],
+                             [[-1.], [0.5], [-3.], [2.], [0.], [0.]]])
+        action_in = np.array([[[1.], [1.], [1.]],
+                              [[2.], [0.], [-1.]],
+                              [[0.], [0.], [0.]],
+                              [[0.5], [-0.5], [0.5]],
+                              [[3.], [3.], [3.]]])
+
+        acc = self.dt*self.dt/(2.*m)
+        vel = self.dt/m
+        exp_u = np.array([[[acc], [vel], [acc], [vel], [acc], [vel]],
+                          [[2.*acc], [2.*vel], [0.*acc], [0.*vel], [-1.*acc], [-1.*vel]],
+                          [[0.*acc], [0.*vel], [0.*acc], [0.*vel], [0.*acc], [0.*vel]],
+                          [[0.5*acc], [0.5*vel], [-0.5*acc], [-0.5*vel], [0.5*acc], [0.5*vel]],
+                          [[3.*acc], [3.*vel], [3.*acc], [3.*vel], [3.*acc], [3.*vel]]])
+
+        exp_x = np.array([[[0.], [0.], [0.], [0.], [0.], [0.]],
+                          [[2.+self.dt], [1.], [5.], [0.], [-1.-2.*self.dt], [-2.]],
+                          [[0.5+0.5*self.dt], [0.5], [0.5+0.5*self.dt], [0.5], [0.5+0.5*self.dt], [0.5]],
+                          [[1.], [0.], [1.], [0.], [1.], [0.]],
+                          [[-1.+0.5*self.dt], [0.5], [-3.+2.*self.dt], [2.], [0.], [0.]]])
+        exp = exp_u + exp_x
+
+        x_pred = model.buildFreeStepGraph("", state_in)
+        u_pred = model.buildActionStepGraph("", action_in)
+        pred = model.buildStepGraph("", state_in, action_in)
+
+        self.assertAllClose(exp_u, u_pred)
+        self.assertAllClose(exp_x, x_pred)
+        self.assertAllClose(exp, pred)
+
+    def test_init_k5_s6_a3_m1d5(self):
+        k = 5
+        s = 6
+        a = 3
+        m = 1.5
+        model = ModelBase(m, self.dt, s, a)
+
+        state_in = np.array([[[-1.], [0.5], [-3.], [2.], [0.], [0.]]])
+        action_in = np.array([[[1.], [1.], [1.]],
+                              [[2.], [0.], [-1.]],
+                              [[0.], [0.], [0.]],
+                              [[0.5], [-0.5], [0.5]],
+                              [[3.], [3.], [3.]]])
+
+        acc = self.dt*self.dt/(2.*m)
+        vel = self.dt/m
+        exp_u = np.array([[[acc], [vel], [acc], [vel], [acc], [vel]],
+                          [[2.*acc], [2.*vel], [0.*acc], [0.*vel], [-1.*acc], [-1.*vel]],
+                          [[0.*acc], [0.*vel], [0.*acc], [0.*vel], [0.*acc], [0.*vel]],
+                          [[0.5*acc], [0.5*vel], [-0.5*acc], [-0.5*vel], [0.5*acc], [0.5*vel]],
+                          [[3.*acc], [3.*vel], [3.*acc], [3.*vel], [3.*acc], [3.*vel]]])
+
+        exp_x = np.array([[[-1+0.5*self.dt], [0.5], [-3.+2.*self.dt], [2.], [0.], [0.]],
+                          [[-1+0.5*self.dt], [0.5], [-3.+2.*self.dt], [2.], [0.], [0.]],
+                          [[-1+0.5*self.dt], [0.5], [-3.+2.*self.dt], [2.], [0.], [0.]],
+                          [[-1+0.5*self.dt], [0.5], [-3.+2.*self.dt], [2.], [0.], [0.]],
+                          [[-1+0.5*self.dt], [0.5], [-3.+2.*self.dt], [2.], [0.], [0.]]])
+
+        exp = exp_u + exp_x
+
+        exp_x = np.expand_dims(exp_x[0, :, :], 0)
+
+        x_pred = model.buildFreeStepGraph("", state_in)
+        u_pred = model.buildActionStepGraph("", action_in)
+        pred = model.buildStepGraph("", state_in, action_in)
+
+        self.assertAllClose(exp_u, u_pred)
+        self.assertAllClose(exp_x, x_pred)
+        self.assertAllClose(exp, pred)
+
+    def test_step3_k5_s6_a3_m1d5(self):
+        k = 5
+        s = 6
+        a = 3
+        m = 1.5
+        model = ModelBase(m, self.dt, s, a)
+
+        state_in = np.array([[[0.], [0.], [0.], [0.], [0.], [0.]],
+                             [[2.], [1.], [5.], [0.], [-1.], [-2.]],
+                             [[0.5], [0.5], [0.5], [0.5], [0.5], [0.5]],
+                             [[1.], [0.], [1.], [0.], [1.], [0.]],
+                             [[-1.], [0.5], [-3.], [2.], [0.], [0.]]])
+        action_in = np.array([[[1.], [1.], [1.]],
+                              [[2.], [0.], [-1.]],
+                              [[0.], [0.], [0.]],
+                              [[0.5], [-0.5], [0.5]],
+                              [[3.], [3.], [3.]]])
+
+        acc = self.dt*self.dt/(2.*m)
+        vel = self.dt/m
+
+        # exp = A**3 * state_in + B*action_in * ( A**2 + A + I)
+
+        # A**3 = | 1 self.dt**3 |
+        #        | 0        1   |
+        # A**2 = | 1 self.dt**2 |
+        #        | 0        1   |
+
+        B_u = np.array([[[1.0*3*(acc+vel*self.dt)], [1.0*vel*3], [1.0*3*(acc+vel*self.dt)], [1.0*vel*3], [1.0*3*(acc+vel*self.dt)], [1.0*vel*3]],
+                        [[2.0*3*(acc+vel*self.dt)], [2.0*vel*3], [0.0*3*(acc+vel*self.dt)], [0.0*vel*3], [-1.0*3*(acc+vel*self.dt)], [-1.0*vel*3]],
+                        [[0.0*3*(acc+vel*self.dt)], [0.0*vel*3], [0.0*3*(acc+vel*self.dt)], [0.0*vel*3], [0.0*3*(acc+vel*self.dt)], [0.0*vel*3]],
+                        [[0.5*3*(acc+vel*self.dt)], [0.5*vel*3], [-0.5*3*(acc+vel*self.dt)], [-0.5*vel*3], [0.5*3*(acc+vel*self.dt)], [0.5*vel*3]],
+                        [[3.0*3*(acc+vel*self.dt)], [3.0*vel*3], [3.0*3*(acc+vel*self.dt)], [3.0*vel*3], [3.0*3*(acc+vel*self.dt)], [3.0*vel*3]]])
+
+        exp_x = np.array([[[0.], [0.], [0.], [0.], [0.], [0.]],
+                          [[2.+self.dt*3], [1.], [5.], [0.], [-1.-2.*self.dt*3], [-2.]],
+                          [[0.5+0.5*self.dt*3], [0.5], [0.5+0.5*self.dt*3], [0.5], [0.5+0.5*self.dt*3], [0.5]],
+                          [[1.], [0.], [1.], [0.], [1.], [0.]],
+                          [[-1.+0.5*self.dt*3], [0.5], [-3.+2.*self.dt*3], [2.], [0.], [0.]]])
+
+        exp = B_u + exp_x
+
+        pred = model.buildStepGraph("", state_in, action_in)
+        pred = model.buildStepGraph("", pred, action_in)
+        pred = model.buildStepGraph("", pred, action_in)
+
+        self.assertAllClose(exp, pred)
+
 
 class TestCost(tf.test.TestCase):
     def setUp(self):
