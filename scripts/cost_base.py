b1f81c88fc013010c12beab071220b9c4e6c23bc
Pierre Nicolay <nicolaypierre.95@gmail.com>
2021-03-18 13:53:19 +0000

    changed cost base to abstract class, implemented static cost which brings the system to a desired goal

--- scripts/cost_base.py
+++ scripts/cost_base.py
@@ -6,30 +6,13 @@ import matplotlib.pyplot as plt
 
 # TODO: compute all constants without tensorflow. Out of the graph computation.
 class CostBase(object):
-    def __init__(self, lam, sigma, goal, tau, Q):
+    def __init__(self, lam, sigma, tau):
 
         with tf.name_scope("Cost_setup") as cs:
             self.lam = lam
             self.tau = tau
             s = tf.convert_to_tensor(sigma, dtype=tf.float64)
             self.invSig = tf.linalg.inv(s)
-            self.setGoal(goal)
-            self.Q = tf.convert_to_tensor(Q, dtype=tf.float64)
-
-
-    def setGoal(self, goal):
-        self.goal = tf.convert_to_tensor(goal, dtype=tf.float64)
-
-
-    def getGoal(self):
-        return self.goal.numpy()
-
-
-    def advanceGoal(self, scope, next):
-        with tf.name_scope("advance_goal") as ag:
-            # shapes: in [s_dim, 1], out None
-            remain = tf.slice(self.goal, [1, 0, 0], [self.tau-1, -1, -1])
-            self.goal = tf.concat([remain, next], 0)
 
 
     def build_step_cost_graph(self, scope, state, action, noise):
@@ -44,16 +27,10 @@ class CostBase(object):
 
 
     def state_cost(self, scope, state):
-        diff = tf.math.subtract(state, self.goal, name="diff")
-        return tf.linalg.matmul(diff, tf.linalg.matmul(self.Q, diff, name="right"), transpose_a=True, name="left")
+        raise NotImplementedError
 
 
     def action_cost(self, scope, action, noise):
         noise_cost = tf.linalg.matmul(self.invSig, noise, name="noise")
         action_cost = tf.linalg.matmul(action, noise_cost, transpose_a=True, name="action")
-        #print("="*25)
-        #print(noise_cost)
-        #print(action_cost)
-        #print(action)
-        #print("="*25)
         return tf.math.multiply(tf.cast(self.lam, dtype=tf.float64), action_cost)
---
+++ scripts/static_cost.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+from cost_base import CostBase
+
+# TODO: compute all constants without tensorflow. Out of the graph computation.
+class StaticCost(CostBase):
+    def __init__(self, lam, sigma, goal, tau, Q):
+        CostBase.__init__(self, lam, sigma, tau)
+        self.setGoal(goal)
+        self.Q = tf.convert_to_tensor(Q, dtype=tf.float64)
+
+    def state_cost(self, scope, state):
+        diff = tf.math.subtract(state, self.goal, name="diff")
+        return tf.linalg.matmul(diff, tf.linalg.matmul(self.Q, diff, name="right"), transpose_a=True, name="left")
+
+    def setGoal(self, goal):
+        self.goal = tf.convert_to_tensor(goal, dtype=tf.float64)
+
+    def getGoal(self):
+        return self.goal.numpy()
+
+    def advanceGoal(self, scope, next):
+        with tf.name_scope("advance_goal") as ag:
+            # shapes: in [s_dim, 1], out None
+            remain = tf.slice(self.goal, [1, 0, 0], [self.tau-1, -1, -1])
+            self.goal = tf.concat([remain, next], 0)
