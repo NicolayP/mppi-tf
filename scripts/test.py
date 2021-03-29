af0b6548bb1699699e473c782d7225912c8a319b
Pierre Nicolay <nicolaypierre.95@gmail.com>
2021-03-29 10:25:17 +0100

    update test to handle new dictionnary returned value

--- scripts/test.py
+++ scripts/test.py
@@ -202,11 +202,12 @@ class TestCost(tf.test.TestCase):
         Sigma=np.array([[1., 0.], [0., 1.]])
         Q=np.array([[1., 0.], [0., 1.]])
         lam=np.array([1.])
+        gamma = 1.
+        upsilon = 1.
 
-        cost = CostBase(lam, Sigma, 1)
+        cost = CostBase(lam, gamma, upsilon, Sigma, 1)
 
-        exp_a_c = np.array([[[2.]]])
-        exp_s_c = np.array([[[1.]]])
+        exp_a_c = np.array([[[ 0.5*(gamma*(2. + 4.) + lam[0]*(1-1./upsilon)*(0.) ) ]]])
 
 
         with self.assertRaises(NotImplementedError):
@@ -228,11 +229,13 @@ class TestCost(tf.test.TestCase):
                    [0., 0., 0., 10.]])
 
         lam=np.array([1.])
+        lam=np.array([1.])
+        gamma = 1.
+        upsilon = 1.
 
-        cost = CostBase(lam, Sigma, 1)
+        cost = CostBase(lam, gamma, upsilon, Sigma, 1)
 
-        exp_a_c = np.array([[[2.25]]])
-        exp_s_c = np.array([[[51.25]]])
+        exp_a_c = np.array([[[0.5*(gamma*(4.25 + 2.*2.25) + lam[0]*(1-1./upsilon)*(1.25) )]]])
 
         with self.assertRaises(NotImplementedError):
             s_c = cost.state_cost("", state)
@@ -262,11 +265,102 @@ class TestCost(tf.test.TestCase):
                    [0., 0., 10., 0.],
                    [0., 0., 0., 10.]])
         lam=np.array([1.])
+        gamma = 1.
+        upsilon = 1.
 
-        cost = CostBase(lam, Sigma, 1)
+        cost = CostBase(lam, gamma, upsilon, Sigma, 1)
 
-        exp_a_c = np.array([[[2.75]], [[4.3125]], [[-1.65]], [[0]], [[2.25]]])
-        exp_s_c = np.array([[[51.25]], [[52]], [[102]], [[0.]], [[333]]])
+
+        exp_a_c = np.array([[[0.5*(gamma*(4.3125 + 2.*2.75) + lam[0]*(1-1./upsilon)*(5.25) ) ]],
+                            [[0.5*(gamma*(4.3125 + 2.*4.3125) + lam[0]*(1-1./upsilon)*(4.3125) ) ]],
+                            [[0.5*(gamma*(4.3125 + 2.*-1.65) + lam[0]*(1-1./upsilon)*(5.04) ) ]],
+                            [[0.5*(gamma*(4.3125 + 2.*0) + lam[0]*(1-1./upsilon)*(0) ) ]],
+                            [[0.5*(gamma*(4.3125 + 2.*2.25) + lam[0]*(1-1./upsilon)*(10.25) ) ]]])
+
+
+        with self.assertRaises(NotImplementedError):
+            s_c = cost.state_cost("", state)
+
+        a_c = cost.action_cost("", action, noise)
+
+        self.assertAllClose(exp_a_c, a_c)
+
+    def testStepCost_s4_a3_l10_g2_u3(self):
+
+        state=np.array([[[0.], [0.5], [2.], [0.]],
+                        [[0.], [2.], [0.], [0.]],
+                        [[10.], [2.], [2.], [3.]],
+                        [[1.], [1.], [1.], [2.]],
+                        [[3.], [4.], [5.], [6.]]])
+        goal=np.array([[1.], [1.], [1.], [2.]])
+        action=np.array([[0.5], [2.], [0.25]])
+        noise=np.array([[[0.5], [1.], [2.]],
+                        [[0.5], [2.], [0.25]],
+                        [[-2], [-0.2], [-1]],
+                        [[0.], [0.], [0.]],
+                        [[1.], [0.5], [3.]]])
+        Sigma=np.array([[1., 0., 0.],
+                        [0., 1., 0.],
+                        [0., 0., 1.]])
+        Q=np.array([[1., 0., 0., 0.],
+                   [0., 1., 0., 0.],
+                   [0., 0., 10., 0.],
+                   [0., 0., 0., 10.]])
+        lam=np.array([10.])
+        gamma = 2.
+        upsilon = 3.
+
+        cost = CostBase(lam, gamma, upsilon, Sigma, 1)
+
+
+        exp_a_c = np.array([[[0.5*(gamma*(4.3125 + 2.*2.75) + lam[0]*(1-1./upsilon)*(5.25) ) ]],
+                            [[0.5*(gamma*(4.3125 + 2.*4.3125) + lam[0]*(1-1./upsilon)*(4.3125) ) ]],
+                            [[0.5*(gamma*(4.3125 + 2.*-1.65) + lam[0]*(1-1./upsilon)*(5.04) ) ]],
+                            [[0.5*(gamma*(4.3125 + 2.*0) + lam[0]*(1-1./upsilon)*(0) ) ]],
+                            [[0.5*(gamma*(4.3125 + 2.*2.25) + lam[0]*(1-1./upsilon)*(10.25) ) ]]])
+
+
+        with self.assertRaises(NotImplementedError):
+            s_c = cost.state_cost("", state)
+
+        a_c = cost.action_cost("", action, noise)
+
+        self.assertAllClose(exp_a_c, a_c)
+
+    def testStepCost_s4_a3_l15_g20_u30(self):
+
+        state=np.array([[[0.], [0.5], [2.], [0.]],
+                        [[0.], [2.], [0.], [0.]],
+                        [[10.], [2.], [2.], [3.]],
+                        [[1.], [1.], [1.], [2.]],
+                        [[3.], [4.], [5.], [6.]]])
+        goal=np.array([[1.], [1.], [1.], [2.]])
+        action=np.array([[0.5], [2.], [0.25]])
+        noise=np.array([[[0.5], [1.], [2.]],
+                        [[0.5], [2.], [0.25]],
+                        [[-2], [-0.2], [-1]],
+                        [[0.], [0.], [0.]],
+                        [[1.], [0.5], [3.]]])
+        Sigma=np.array([[1., 0., 0.],
+                        [0., 1., 0.],
+                        [0., 0., 1.]])
+        Q=np.array([[1., 0., 0., 0.],
+                   [0., 1., 0., 0.],
+                   [0., 0., 10., 0.],
+                   [0., 0., 0., 10.]])
+        lam=np.array([15.])
+        gamma = 20.
+        upsilon = 30.
+
+        cost = CostBase(lam, gamma, upsilon, Sigma, 1)
+
+
+        exp_a_c = np.array([[[0.5*(gamma*(4.3125 + 2.*2.75) + lam[0]*(1-1./upsilon)*(5.25) ) ]],
+                            [[0.5*(gamma*(4.3125 + 2.*4.3125) + lam[0]*(1-1./upsilon)*(4.3125) ) ]],
+                            [[0.5*(gamma*(4.3125 + 2.*-1.65) + lam[0]*(1-1./upsilon)*(5.04) ) ]],
+                            [[0.5*(gamma*(4.3125 + 2.*0) + lam[0]*(1-1./upsilon)*(0) ) ]],
+                            [[0.5*(gamma*(4.3125 + 2.*2.25) + lam[0]*(1-1./upsilon)*(10.25) ) ]]])
+
 
         with self.assertRaises(NotImplementedError):
             s_c = cost.state_cost("", state)
@@ -289,22 +383,23 @@ class TestStaticCost(tf.test.TestCase):
         Sigma=np.array([[1., 0.], [0., 1.]])
         Q=np.array([[1., 0.], [0., 1.]])
         lam=np.array([1.])
+        gamma = 1.
+        upsilon = 1.
 
-        cost = StaticCost(lam, Sigma, goal, 1, Q)
+        cost = StaticCost(lam, gamma, upsilon, Sigma, goal, 1, Q)
 
-        exp_a_c = np.array([[[2.]]])
+        exp_a_c = np.array([[[ 0.5*(gamma*(2. + 4.) + lam[0]*(1-upsilon)*(0.) ) ]]])
         exp_s_c = np.array([[[1.]]])
 
 
         exp_c = exp_a_c + exp_s_c
 
         a_c = cost.action_cost("", action, noise)
-        s_c = cost.state_cost("", state)
-        c, _, _ = cost.build_step_cost_graph("", state, action, noise)
+        c_dict = cost.build_step_cost_graph("", state, action, noise)
 
         self.assertAllClose(exp_a_c, a_c)
-        self.assertAllClose(exp_s_c, s_c)
-        self.assertAllClose(exp_c, c)
+        self.assertAllClose(exp_s_c, c_dict["state_cost"])
+        self.assertAllClose(exp_c, c_dict["cost"])
 
     def testStepStaticCost_s4_a2_l1(self):
 
@@ -319,23 +414,24 @@ class TestStaticCost(tf.test.TestCase):
                    [0., 0., 0., 10.]])
 
         lam=np.array([1.])
+        gamma = 1.
+        upsilon = 1.
 
-        cost = StaticCost(lam, Sigma, goal, 1, Q)
+        cost = StaticCost(lam, gamma, upsilon, Sigma, goal, 1, Q)
 
-        exp_a_c = np.array([[[2.25]]])
+        exp_a_c = np.array([[[0.5*(gamma*(4.25 + 2.*2.25) + lam[0]*(1-upsilon)*(1.25) )]]])
+
         exp_s_c = np.array([[[51.25]]])
 
         exp_c = exp_a_c + exp_s_c
 
 
         a_c = cost.action_cost("", action, noise)
-        s_c = cost.state_cost("", state)
-        c, _, _ = cost.build_step_cost_graph("", state, action, noise)
-
+        c_dict = cost.build_step_cost_graph("", state, action, noise)
 
         self.assertAllClose(exp_a_c, a_c)
-        self.assertAllClose(exp_s_c, s_c)
-        self.assertAllClose(exp_c, c)
+        self.assertAllClose(exp_s_c, c_dict["state_cost"])
+        self.assertAllClose(exp_c, c_dict["cost"])
 
     def testStepStaticCost_s4_a3_l1(self):
 
@@ -359,21 +455,27 @@ class TestStaticCost(tf.test.TestCase):
                    [0., 0., 10., 0.],
                    [0., 0., 0., 10.]])
         lam=np.array([1.])
+        gamma = 1.
+        upsilon = 1.
 
-        cost = StaticCost(lam, Sigma, goal, 1, Q)
+        cost = StaticCost(lam, gamma, upsilon, Sigma, goal, 1, Q)
 
-        exp_a_c = np.array([[[2.75]], [[4.3125]], [[-1.65]], [[0]], [[2.25]]])
+        exp_a_c = np.array([[[0.5*(gamma*(4.3125 + 2.*2.75) + lam[0]*(1-upsilon)*(5.25) ) ]],
+                            [[0.5*(gamma*(4.3125 + 2.*4.3125) + lam[0]*(1-upsilon)*(4.3125) ) ]],
+                            [[0.5*(gamma*(4.3125 + 2.*-1.65) + lam[0]*(1-upsilon)*(5.04) ) ]],
+                            [[0.5*(gamma*(4.3125 + 2.*0) + lam[0]*(1-upsilon)*(0) ) ]],
+                            [[0.5*(gamma*(4.3125 + 2.*2.25) + lam[0]*(1-upsilon)*(10.25) ) ]]])
+
         exp_s_c = np.array([[[51.25]], [[52]], [[102]], [[0.]], [[333]]])
 
         exp_c = exp_a_c + exp_s_c
 
         a_c = cost.action_cost("", action, noise)
-        s_c = cost.state_cost("", state)
-        c, _, _ = cost.build_step_cost_graph("", state, action, noise)
+        c_dict = cost.build_step_cost_graph("", state, action, noise)
 
         self.assertAllClose(exp_a_c, a_c)
-        self.assertAllClose(exp_s_c, s_c)
-        self.assertAllClose(exp_c, c)
+        self.assertAllClose(exp_s_c, c_dict["state_cost"])
+        self.assertAllClose(exp_c, c_dict["cost"])
 
 
 class TestElipseCost(tf.test.TestCase):
@@ -389,14 +491,17 @@ class TestElipseCost(tf.test.TestCase):
         self.m_s=1
         self.m_v=1
 
-
     def testStepElipseCost_s4_l1_k1(self):
 
         state=np.array([[[0.], [0.5], [1.], [0.]]])
         sigma=np.array([[1., 0.], [0., 1.]])
         lam=np.array([1.])
+        gamma = 1.
+        upsilon = 1.
 
         cost = ElipseCost(lam,
+                          gamma,
+                          upsilon,
                           sigma,
                           self.tau,
                           self.a, self.b, self.cx, self.cy,
@@ -404,10 +509,10 @@ class TestElipseCost(tf.test.TestCase):
 
         exp_s_c = np.array([[[0.25]]])
 
-        s_c = cost.state_cost("", state)
+        s_c_dict = cost.state_cost("", state)
 
 
-        self.assertAllClose(exp_s_c, s_c)
+        self.assertAllClose(exp_s_c, s_c_dict["state_cost"])
 
     def testStepElipseCost_s4_l1_k5(self):
 
@@ -420,8 +525,12 @@ class TestElipseCost(tf.test.TestCase):
                         [0., 1., 0.],
                         [0., 0., 1.]])
         lam=np.array([1.])
+        gamma = 1.
+        upsilon = 1.
 
         cost = ElipseCost(lam,
+                          gamma,
+                          upsilon,
                           sigma,
                           self.tau,
                           self.a, self.b, self.cx, self.cy,
@@ -433,8 +542,8 @@ class TestElipseCost(tf.test.TestCase):
                             [[1+1.5278640450004208]],
                             [[33+38.57779489814404]]])
 
-        s_c = cost.state_cost("", state)
-        self.assertAllClose(exp_s_c, s_c)
+        s_c_dict = cost.state_cost("", state)
+        self.assertAllClose(exp_s_c, s_c_dict["state_cost"])
 
 
 class TestController(tf.test.TestCase):
@@ -446,6 +555,8 @@ class TestController(tf.test.TestCase):
         self.dt = 0.01
         self.mass = 1.
         self.lam = 1.
+        self.gamma = 1.
+        self.upsilon = 1.
 
         self.goal = np.array([[0.], [1.]])
         self.sigma = np.array([[1., 0.], [0., 1.]])
@@ -463,7 +574,7 @@ class TestController(tf.test.TestCase):
 
         self.a = np.array([[[1.], [0.5]], [[2.3], [4.5]], [[2.1], [-0.4]]])
         model = ModelBase(self.mass, self.dt, self.s_dim, self.a_dim)
-        cost = StaticCost(self.lam, self.sigma, self.goal, self.tau, self.Q)
+        cost = StaticCost(self.lam, self.gamma, self.upsilon, self.sigma, self.goal, self.tau, self.Q)
         self.cont = ControllerBase(model,
                                    cost,
                                    self.k,
@@ -525,7 +636,8 @@ class TestController(tf.test.TestCase):
                               [0.034951787275480706*1 + 3.1871904480408675e-05*3 + 0.7020254138530686*0.5 + 0.2582607169364174*(-0.4) + 0.004730210030553017*0]]])
 
         b = self.cont.beta("", self.c)
-        e_arg = self.cont.expArg("", self.c, b)
+        arg = self.cont.normArg("", self.c, b, False)
+        e_arg = self.cont.expArg("", arg)
         e = self.cont.exp("", e_arg)
         nab = self.cont.nabla("", e)
         w = self.cont.weights("", e, nab)
