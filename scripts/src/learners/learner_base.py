7ac478f22c5bb0069d3d98ba8a0026b7c906ec4e
Pierre Nicolay <pon1@hw.ac.uk>
2022-03-10 16:18:31 +0000

    augmented dataset with random noise, cleaner and clearer logging mechanism

--- scripts/src/learners/learner_base.py
+++ scripts/src/learners/learner_base.py
@@ -11,8 +11,10 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 import copy
+import time as t
 
 from ..model import copy_model
+
 
 class LearnerBase(tf.Module):
     # PUBLIC
@@ -45,7 +47,7 @@ class LearnerBase(tf.Module):
 
         if self.log:
             stamp = datetime.now().strftime("%Y.%m.%d-%H:%M:%S")
-            self.logdir = os.path.join(logPath, "learner", stamp)
+            self.logdir = os.path.join(logPath, stamp)
             self.writer = tf.summary.create_file_writer(self.logdir)
             self._save_graph()
 
@@ -82,20 +84,29 @@ class LearnerBase(tf.Module):
         init_weights = self.model.get_weights()
         
         learningRate = np.linspace(0.0001, 0.1, 10)
+        beta1 = np.linspace(0.999, 0.009, 10)
+        beta2 = np.linspace(0.999, 0.009, 10)
+        sigma = np.linspace(0.01, 0.001, 10)
         #learningRate = np.linspace(0.0001, 0.1, 2)
         batchSize = np.array([-1])
-        epoch = np.array([50, 100, 500])
+        epoch = np.array([250])
         #epoch = np.array([2])
-        
+
         writers = []
         for lr in learningRate:
             lr_writers = []
-            for bs in batchSize:
-                logdir = os.path.join(self.logdir, "-lr-{:4f}".format(lr), "-bs-{}".format(bs))
-                lr_writers.append(tf.summary.create_file_writer(logdir))
+            for s in sigma:
+                s_writers = []
+                for bs in batchSize:
+                    logdir = os.path.join(self.logdir,
+                                          "-lr-{:4f}".format(lr),
+                                          "-bs-{:4f}".format(bs),
+                                          "-s-{:4f}".format(s))
+                    s_writers.append(tf.summary.create_file_writer(logdir))
+                lr_writers.append(s_writers)
             writers.append(lr_writers)
-        
-        k = 10
+
+        #k = 10
         k = 2
         self.stats()
 
@@ -103,21 +114,32 @@ class LearnerBase(tf.Module):
         valTransErr = []
         for e in epoch:
             for lr, lr_writers in zip(learningRate, writers):
-                for bs, writer in zip(batchSize, lr_writers):
-                    self.k_fold_validation(learningRate=lr,
-                                           batchSize=bs,
-                                           epoch=e, k=k,
-                                           val=(trajs, actionSeqs),
-                                           writer=writer)
-
-                    self.train_all(learningRate=lr, batchSize=bs,
-                                   epoch=e, val=(trajs, actionSeqs),
-                                   writer=writer)
-                    err, transErr = self.validate(self.model, actionSeqs, trajs, plot=True, transition=True, split=False)
-                    valErr.append(np.expand_dims(err, axis=0))
-                    valTransErr.append(np.expand_dims(transErr, axis=0))
-                    print("lr: {}, e: {}, validation error: {}, transition error: {}".format(lr, e, err.numpy(), transErr.numpy()))
-                    self.model.update_weights(init_weights, msg=False)
+                for s, s_writers in zip(sigma, lr_writers):
+                    self.sigma = s
+                    for bs, writer in zip(batchSize, s_writers):
+                        print("K-fold")
+                        start = t.perf_counter()
+                        self.k_fold_validation(learningRate=lr,
+                                            batchSize=bs,
+                                            epoch=e, k=k,
+                                            val=(trajs, actionSeqs),
+                                            writer=writer)
+                        end = t.perf_counter()
+                        print("K-Fold done in {:.4f} s".format(end-start))
+                        
+                        print("Train")
+                        start = t.perf_counter()
+                        self.train_all(learningRate=lr, batchSize=bs,
+                                    epoch=e, val=(trajs, actionSeqs),
+                                    writer=writer)
+                        end = t.perf_counter()
+                        print("Training done in {:.4f} s".format(end-start))
+                        
+                        err, transErr = self.validate(self.model, actionSeqs, trajs, plot=True, transition=True, split=False)
+                        valErr.append(np.expand_dims(err, axis=0))
+                        valTransErr.append(np.expand_dims(transErr, axis=0))
+                        print("lr: {}, e: {}, validation error: {}, transition error: {}".format(lr, e, err.numpy(), transErr.numpy()))
+                        self.model.update_weights(init_weights, msg=False)
         valErr = np.concatenate(valErr, axis=0)
         print("Best mean:", np.max(valErr))
 
@@ -128,7 +150,7 @@ class LearnerBase(tf.Module):
         (X, y) = self.model.prepare_training_data(data['obs'],
                                                   data['next_obs'],
                                                   data['act'])
-        self.train(X, y, epoch=epoch, learningRate=learningRate, val=val, writer=writer)
+        self.train(X, y, epoch=epoch, learningRate=learningRate, val=val, writer=writer, augment=True)
 
     def k_fold_validation(self, k=10, learningRate=0.1, batchSize=32,
                           epoch=100, val=None, writer=None):
@@ -137,6 +159,7 @@ class LearnerBase(tf.Module):
         (X, y) = self.model.prepare_training_data(data['obs'],
                                                   data['next_obs'],
                                                   data['act'])
+        #(X, y) = self.augmented_data(self.model)
         X = X.numpy()
         y = y.numpy()
 
@@ -147,23 +170,22 @@ class LearnerBase(tf.Module):
 
         # prepare models copy
         m = copy_model(self.model)
-        
 
         models = [copy_model(self.model) for i in range(k)]
         init_weights = self.model.get_weights()
         for m in models:
-            m.update_weights(init_weights, msg=False) 
+            m.update_weights(init_weights, msg=False)
         # perpare optimizers (not necessary I think but not sure if adam hasn't a form of internal memory about gradients.)
         optimizers = [tf.optimizers.Adam(learning_rate=learningRate) for i in range(k)]
 
         for e in range(epoch):
             lossesTrain, lossesTest = self.kfold_step(models=models, optimizers=optimizers, data=kFoldData)
             logScope="kfold/e{}".format(epoch)
-            self._log_hist(writer, "train/" + logScope, lossesTrain, e)
-            self._log_hist(writer, "test/" + logScope, lossesTest, e)
+            self._log_hist(writer, "Train/" + logScope, lossesTrain, e)
+            self._log_hist(writer, "Test/" + logScope, lossesTest, e)
             if e % 10 == 0 and (val is not None):
                 lossFold = self.validate_fold(models, val)
-                self._log_hist(writer, "val/" + logScope, lossFold, e)
+                self._log_hist(writer, "Val/" + logScope, lossFold, e)
 
     def kfold_step(self, models, optimizers, data):
         k = len(data)
@@ -193,7 +215,8 @@ class LearnerBase(tf.Module):
         
         return tf.concat(losses, axis=0)
 
-    def validate(self, model, actionSeqs, gtTrajs, plot=False, transition=False, split=False):
+    def validate(self, model, actionSeqs, gtTrajs,
+                 plot=False, transition=False, split=False, norm=False):
         '''
             computes the error of the model for a number of trajectories with
             the matching action sequences.
@@ -206,14 +229,18 @@ class LearnerBase(tf.Module):
                 - gtTrajs: Tensor of the ground truth trajectories.
                     Shape [k, tau, 13]
 
-                - plot: bool (default:False) if true, also plots
+                - plot: bool (default: False) if true, also plots
                     the first predicted trajectory id:0
 
-                - transition: bool (default:False) if true, also
+                - transition: bool (default: False) if true, also
                     computes the individual prediction error.
 
-                - split: bool (default:False) if true, also computes
+                - split: bool (default: False) if true, also computes
                     the error for every output dimension individually
+
+                - norm: bool (default: False) if true, also computes
+                    the error for the normalized data, including trajectory error.
+
             - output:
             ---------
                 - L(nn(actionSeqs), trajs), the loss between the predicted trajectory
@@ -233,11 +260,17 @@ class LearnerBase(tf.Module):
                     nextState = model.build_step_graph(s, state, action)
             state = nextState
             trajs.append(np.expand_dims(state, axis=1))
-        
+
         trajs = np.squeeze(np.concatenate(trajs, axis=1), axis=-1)
         errSplit = tf.reduce_mean(tf.math.squared_difference(trajs, gtTrajs),
                                   name="loss", axis=[0, 1])
         err = tf.reduce_mean(errSplit)
+
+        trajsNorm = self.norm_trajs(model, trajs)
+        gtTrajsNorm = self.norm_trajs(model, gtTrajs)
+        errSplitNorm = tf.reduce_mean(tf.math.squared_difference(trajsNorm, gtTrajsNorm),
+                                  name="loss", axis=[0, 1])
+        errNorm = tf.reduce_mean(errSplitNorm)
 
         if transition:
             actions = actionSeqs[:, :-1, :]
@@ -248,8 +281,16 @@ class LearnerBase(tf.Module):
             states = tf.reshape(states, shape=((tau-1)*k, gtTrajs.shape[2], 1))
             nextStates = tf.reshape(nextStates, shape=((tau-1)*k, gtTrajs.shape[2], 1))
 
-            (X, y) = model.prepare_training_data(states, nextStates, actions)
-            pred = model._predict_nn("transition", X)
+            (XNorm, yNorm) = model.prepare_training_data(states, nextStates, actions)
+            predNorm = model._predict_nn("transition", XNorm)
+
+            transErrSplitNorm = tf.reduce_mean(tf.math.squared_difference(predNorm, yNorm),
+                                      name="transition-loss", axis=0)
+            transErrNorm = tf.reduce_mean(transErrSplitNorm)
+
+            pred = model.denormalizeY(predNorm)
+            y = model.denormalizeY(yNorm)
+
             transErrSplit = tf.reduce_mean(tf.math.squared_difference(pred, y),
                                       name="transition-loss", axis=0)
             transErr = tf.reduce_mean(transErrSplit)
@@ -257,28 +298,80 @@ class LearnerBase(tf.Module):
         if plot:
             self.plot_seq(trajs[0], gtTrajs[0])
 
+        if transition and split and norm:
+            return err, errNorm, errSplit, errSplitNorm, transErr, transErrNorm, transErrSplit, transErrSplitNorm
+        
         if transition and split:
             return err, errSplit, transErr, transErrSplit
+        
+        if transition and norm:
+            return err, errNorm, transErr, transErrNorm
+        
+        if split and norm:
+            return err, errNorm, errSplit, errSplitNorm
+
         if transition:
             return err, transErr
+
         if split:
             return err, errSplit
+        
+        if norm:
+            return err, errNorm
+
         return err
 
-    def train(self, X, y, epoch=1, learningRate=0.1, val=None, writer=None):
+    def train(self, X, y, epoch=1, learningRate=0.1, augment=False, val=None, writer=None):
         for e in range(epoch):
+            if augment:
+                XEp, yEp = self.augment_data(X, y, sigma=self.sigma)
+            else:
+                XEp, yEp = X, y
             scope = "e{}".format(epoch)
-            loss, lossSplit, grad = self._train_step(self.model, self.optimizer, X, y, split=True)
-            self._log_scalar(writer, "train/" + scope, loss, e)
-            #self._log_hist(writer, "grad/" + scope, grad, e)
-            self._log_split(writer, "trainSplit/" + scope, lossSplit, e)
+            loss, lossNorm, lossSplit, lossSplitNorm, grad = self._train_step(self.model, self.optimizer, XEp, yEp, split=True, norm=True)
+            self._log_scalar(writer, "E{}-Train/".format(epoch), loss, e)
+            self._log_scalar(writer, "E{}-Train-Norm/".format(epoch), lossNorm, e)
+            self._log_grad_weights(writer, "E{}-".format(epoch), grad,
+                                   self.model.get_weights(), self.model.nn.layers, e)
+            self._log_split(writer, "E{}-Train-Split/".format(epoch), lossSplit, e)
+            self._log_split(writer, "E{}-Train-Split-Norm/".format(epoch), lossSplitNorm, e)
 
             if (val is not None) and (e % 10 == 0):
-                valLoss, valLossSplit, transValLoss, transValLossSplit = self.validate(self.model, val[1], val[0], transition=True, split=True)
-                self._log_scalar(writer, "valTotal/" + scope, valLoss, e)
-                self._log_scalar(writer, "valTransition/" + scope, transValLoss, e)
-                self._log_split(writer, "valSplit/" + scope, valLossSplit, e)
-                self._log_split(writer, "valTransitionSplit/" + scope, transValLossSplit, e)
+                trainPred = self.model._predict_nn('val-test', X)
+                lossTrainSet = tf.reduce_mean(tf.math.squared_difference(trainPred, y),
+                                              name="test-loss-norm")
+
+                valErrs = self.validate(self.model, val[1], val[0],
+                                        transition=True, split=True, norm=True)
+
+                self._log_scalar(writer, "E{}-Val-Train-Set-Norm/".format(epoch), lossTrainSet, e)
+                self._log_scalar(writer, "E{}-Val-Total/".format(epoch), valErrs[0], e)
+                self._log_scalar(writer, "E{}-Val-Total-Norm/".format(epoch), valErrs[1], e)
+
+                self._log_split(writer, "E{}-Val-Split/".format(epoch), valErrs[2][-6:], e)
+                self._log_split(writer, "E{}-Val-Split-Norm/".format(epoch), valErrs[3][-6:], e)
+
+                self._log_scalar(writer, "E{}-Val-Transition/".format(epoch), valErrs[4], e)
+                self._log_scalar(writer, "E{}-Val-Transition-Norm/".format(epoch), valErrs[5], e)
+
+                self._log_split(writer, "E{}-Val-Transition-Split/".format(epoch), valErrs[6], e)
+                self._log_split(writer, "E{}-Val-Transition-Split-Norm/".format(epoch), valErrs[7], e)
+
+    def norm_trajs(self, model, trajs):
+        Xmean = self.model.Xmean
+        Xstd = self.model.Xstd
+        mean = np.zeros(shape=(12,))
+        std = np.ones(shape=(12,))
+        mean[3:] = Xmean[:9]
+        std[3:] = Xstd[:9]
+        normTrajs = np.zeros(shape=(trajs.shape[0], trajs.shape[1], trajs.shape[2]-1))
+        for k in range(trajs.shape[0]):
+            eulerTraj = model.to_euler(tf.expand_dims(trajs[k], axis=-1))
+            eulerTraj = tf.squeeze(eulerTraj, axis=-1)
+            normEulerTraj = (eulerTraj - mean)/std
+            normTrajs[k, :, :] = normEulerTraj
+
+        return normTrajs
 
     def plot_seq(self, traj, gtTraj):
         fig, axs = plt.subplots(figsize=(20, 10), nrows=2, ncols=7)
@@ -354,20 +447,52 @@ class LearnerBase(tf.Module):
                    ncol=2, borderaxespad=0., bbox_transform=fig.transFigure)
         plt.show()
 
+    def augment_data(self, X, y, samples=5, sigma=0.001):
+        XAug = tf.repeat(X, samples, axis=0)
+        yAug = tf.repeat(y, samples, axis=0)
+
+        noise = tf.random.normal(shape=(XAug.shape),
+                                 stddev=sigma,
+                                 mean=0.,
+                                 dtype=tf.float64,
+                                 seed=1)
+
+
+
+        XAug = XAug + noise
+
+        XAug = self.model.denormalizeX(XAug)
+        yAug = self.model.denormalizeY(yAug)
+
+        return (XAug, yAug)
     # PRIVATE
-    def _train_step(self, model, optimizer, X, y, split=False):
+    def _train_step(self, model, optimizer, Xnorm, Ynorm, split=False, norm=False):
         # If batchSize = -1, feed in the entire batch
         with tf.GradientTape() as tape:
-            pred = model._predict_nn("train", X)
-            lossSplit = tf.reduce_mean(tf.math.squared_difference(pred, y), axis=0)
-            loss = tf.reduce_mean(tf.math.squared_difference(pred, y),
-                                  name="loss")
-            grads = tape.gradient(loss, model.weights())
+            predNorm = model._predict_nn("train", Xnorm)
+            lossSplitNorm = tf.reduce_mean(tf.math.squared_difference(predNorm, Ynorm), axis=0)
+            lossNorm = tf.reduce_mean(tf.math.squared_difference(predNorm, Ynorm),
+                                      name="lossNorm")
+
+            grads = tape.gradient(lossNorm, model.weights())
             optimizer.apply_gradients(zip(grads,
                                           model.weights()))
 
+            pred = model.denormalizeY(predNorm)
+            y = model.denormalizeY(Ynorm)
+            lossSplit = tf.reduce_mean(tf.math.squared_difference(pred, y), axis=0)
+            loss = tf.reduce_mean(tf.math.squared_difference(pred, y),
+                                  name="loss")
+
+            if split and norm:
+                return loss, lossNorm, lossSplit, lossSplitNorm, grads
+
             if split:
                 return loss, lossSplit, grads
+
+            if norm:
+                return loss, lossNorm, grads
+
             return loss, grads
 
     def _save_graph(self):
@@ -381,6 +506,20 @@ class LearnerBase(tf.Module):
                     ).graph
             # visualize
             summary_ops_v2.graph(graph.as_graph_def())
+
+    def _log_grad_weights(self, writer, scope, gradList, weightsList, layerList, step):
+        i = 0
+        for layer in layerList:
+            w_grad = tf.reshape(gradList[i], shape=(-1,))
+            b_grad = tf.reshape(gradList[i+1], shape=(-1,))
+            w = tf.reshape(weightsList[i], shape=(-1,))
+            b = tf.reshape(weightsList[i+1], shape=(-1,))
+
+            self._log_hist(writer, scope + "grad/" + layer._name + "/weights", w_grad, step)
+            self._log_hist(writer, scope + "grad/" + layer._name + "/bias", b_grad, step)
+            self._log_hist(writer, scope + "weights/" + layer._name + "/weights", w, step)
+            self._log_hist(writer, scope + "weights/" + layer._name + "/bias", b, step)
+            i += 2
 
     def _log_hist(self, writer, scope, tensor, step):
         if self.log:
