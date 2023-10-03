1a9456353198d9cf0be9762acbe9d12eadded33f
Snow <nicolaypierre.95@gmail.com>
2023-10-03 14:38:51 +0100

    renamed test_training to test_training_utils, test 1 training step for NN Model

--- tests/test_training.py
+++
@@ -1,6 +0,0 @@
-import unittest
-import torch
-
-
-if __name__ == '__main__':
-    unittest.main()
---
+++ tests/test_training_utils.py
@@ -0,0 +1,151 @@
+import unittest
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from scripts.utils.utils import get_device
+from scripts.training.loss_fct import TrajLoss
+from scripts.models.nn_auv import AUVTraj
+from scripts.training.learning_utils import get_dataloader_model_input, train_step
+
+
+class TestTrainStepNN_CPU(unittest.TestCase):
+    def setUp(self):
+        data_dir = "data/tests"
+        nb_files, steps, history = 3, 3, 2
+        train_params = {"batch_size": 10, "shuffle": True, "num_workers": 8}
+        self.dl = get_dataloader_model_input(data_dir, nb_files, steps, history, train_params)
+        model_param = {"model": {"type": "nn", "se3": True, "history": history}}
+        self.device = get_device(cpu=True)
+        self.model = AUVTraj(model_param).to(self.device)
+        self.loss = TrajLoss().to(self.device)
+        self.optim = torch.optim.Adam(self.model.parameters(), lr=1e-4)
+        self.epochs = 2
+    
+    def test_train_step(self):
+        train_step(self.dl, self.model, self.loss, self.optim, None, self.epochs, self.device)
+
+    def test_val_step(self):
+        pass
+
+    def test_train(self):
+        pass
+
+
+class TestTrainStepNN_GPU(unittest.TestCase):
+    def setUp(self):
+        data_dir = "data/tests"
+        nb_files, steps, history = 3, 3, 2
+        train_params = {"batch_size": 10, "shuffle": True, "num_workers": 8}
+        self.dl = get_dataloader_model_input(data_dir, nb_files, steps, history, train_params)
+        model_param = {"model": {"type": "nn", "se3": True, "history": history}}
+        self.device = get_device(0)
+        self.model = AUVTraj(model_param).to(self.device)
+        self.loss = TrajLoss().to(self.device)
+        self.optim = torch.optim.Adam(self.model.parameters(), lr=1e-4)
+        self.epochs = 2
+    
+    def test_train_step(self):
+        train_step(self.dl, self.model, self.loss, self.optim, None, self.epochs, self.device)
+
+    def test_val_step(self):
+        pass
+
+    def test_train(self):
+        pass
+
+
+# class TestTrainStepRNN_CPU(unittest.TestCase):
+#     def setUp(self):
+#         data_dir = "data/tests"
+#         nb_files, steps, history = 3, 3, 2
+#         train_params = {"batch_size": 10, "shuffle": True, "num_workers": 8}
+#         self.dl = get_dataloader_model_input(data_dir, nb_files, steps, history, train_params)
+        
+#         self.device = get_device(cpu=True)
+#         self.model = AUVTraj().to(self.device)
+#         self.loss = TrajLoss().to(self.device)
+#         self.optim = torch.optim.Adam(self.model.parameters(), lr=1e-4)
+#         self.epochs = 2
+    
+#     def test_train_step(self):
+#         train_step(self.dl, self.model, self.loss, self.optim, None, self.epochs, self.device)
+
+    # def test_val_step(self):
+    #     pass
+
+    # def test_train(self):
+    #     pass
+
+
+# class TestTrainStepRNN_GPU(unittest.TestCase):
+#     def setUp(self):
+#         data_dir = "data/tests"
+#         nb_files, steps, history = 3, 3, 2
+#         train_params = {"batch_size": 10, "shuffle": True, "num_workers": 8}
+#         self.dl = get_dataloader_model_input(data_dir, nb_files, steps, history, train_params)
+        
+#         self.device = get_device(0)
+#         self.model = AUVTraj().to(self.device)
+#         self.loss = TrajLoss().to(self.device)
+#         self.optim = torch.optim.Adam(self.model.parameters(), lr=1e-4)
+#         self.epochs = 2
+    
+#     def test_train_step(self):
+#         train_step(self.dl, self.model, self.loss, self.optim, None, self.epochs, self.device)
+
+    # def test_val_step(self):
+    #     pass
+
+    # def test_train(self):
+    #     pass
+
+
+# class TestTrainStepLSTM_GPU(unittest.TestCase):
+#     def setUp(self):
+#         data_dir = "data/tests"
+#         nb_files, steps, history = 3, 3, 2
+#         train_params = {"batch_size": 10, "shuffle": True, "num_workers": 8}
+#         self.dl = get_dataloader_model_input(data_dir, nb_files, steps, history, train_params)
+        
+#         self.device = get_device(0)
+#         self.model = AUVTraj().to(self.device)
+#         self.loss = TrajLoss().to(self.device)
+#         self.optim = torch.optim.Adam(self.model.parameters(), lr=1e-4)
+#         self.epochs = 2
+    
+#     def test_train_step(self):
+#         train_step(self.dl, self.model, self.loss, self.optim, None, self.epochs, self.device)
+
+    # def test_val_step(self):
+    #     pass
+
+    # def test_train(self):
+    #     pass
+
+
+# class TestTrainStepLSTM_GPU(unittest.TestCase):
+#     def setUp(self):
+#         data_dir = "data/tests"
+#         nb_files, steps, history = 3, 3, 2
+#         train_params = {"batch_size": 10, "shuffle": True, "num_workers": 8}
+#         self.dl = get_dataloader_model_input(data_dir, nb_files, steps, history, train_params)
+        
+#         self.device = get_device(0)
+#         self.model = AUVTraj().to(self.device)
+#         self.loss = TrajLoss().to(self.device)
+#         self.optim = torch.optim.Adam(self.model.parameters(), lr=1e-4)
+#         self.epochs = 2
+    
+#     def test_train_step(self):
+#         train_step(self.dl, self.model, self.loss, self.optim, None, self.epochs, self.device)
+
+    # def test_val_step(self):
+    #     pass
+
+    # def test_train(self):
+    #     pass
+
+
+if __name__ == '__main__':
+    unittest.main()
